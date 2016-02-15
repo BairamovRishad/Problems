cc4d1e15a90f6cb673b50dbebafe91503f137a85
Rishad Bairamov <rbairamov@rightandabove.com>
2016-02-16 01:36:00 +0300

    Add the solution to the AnagramFinder's problem and test it

---
+++ src/main/java/com/bairamov/anagramfinder/AnagramFinder.java
@@ -0,0 +1,36 @@
+package com.bairamov.anagramfinder;
+
+import java.util.*;
+
+/*
+Find all the anagrams in a vector of words.
+Your function should return a vector of vectors, where each sub-vector is a group of words which are anagrams of each other.
+Words without any anagrams should not be included in the result.
+*/
+
+public class AnagramFinder {
+    public List<List<String>> getAnagramsList(List<String> words) {
+        List<List<String>> anagramsList = new ArrayList<>();
+
+        Map<String, List<String>> anagramsMap = new LinkedHashMap<>();
+
+        for (String word : words) {
+            String sorted = sortString(word);
+
+            if (!anagramsMap.containsKey(sorted)) {
+                anagramsMap.put(sorted, new ArrayList<>());
+            }
+            anagramsMap.get(sorted).add(word);
+        }
+
+        anagramsMap.values().stream().filter(anagrams -> anagrams.size() > 1).forEach(anagramsList::add);
+
+        return anagramsList;
+    }
+
+    private String sortString(String word) {
+        char[] wordLetters = word.toCharArray();
+        Arrays.sort(wordLetters);
+        return String.valueOf(wordLetters);
+    }
+}
---
+++ src/test/java/com/bairamov/anagramfinder/AnagramFinderTest.java
@@ -0,0 +1,46 @@
+package com.bairamov.anagramfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class AnagramFinderTest {
+    private List<String> inputWords;
+    private List<List<String>> expectedAnagramsList;
+
+    public AnagramFinderTest(List<String> input, List<List<String>> expected) {
+        inputWords = input;
+        expectedAnagramsList = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        List<String> input1 = Arrays.asList("veer", "lake", "item", "kale", "mite", "ever");
+        List<List<String>> expected1 = Arrays.asList(
+                Arrays.asList("veer", "ever"),
+                Arrays.asList("lake", "kale"),
+                Arrays.asList("item", "mite")
+        );
+
+        List<String> input2 = Arrays.asList("meat", "mat", "team", "mate", "eat");
+        List<List<String>> expected2 = Collections.singletonList(Arrays.asList("meat", "team", "mate"));
+
+        return Arrays.asList(new Object[][]{
+                {input1, expected1},
+                {input2, expected2}
+        });
+    }
+
+    @Test
+    public void testGetAnagramsList() throws Exception {
+        assertEquals(expectedAnagramsList, new AnagramFinder().getAnagramsList(inputWords));
+    }
+}
