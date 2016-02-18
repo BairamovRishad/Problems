bf3ed56fc0bec703c459ef552d63fcf16b6d407e
BairamovRishad <bayramov.rishad@gmail.com>
2016-02-19 01:16:01 +0300

    Add the solution to the array-transpose's problem and test it

---
+++ src/main/java/com/bairamov/arraytranspose/Arrays.java
@@ -0,0 +1,32 @@
+package com.bairamov.arraytranspose;
+
+/*
+Implement the matrix transposition function.
+Matrices are presented as arrays of arrays, where internal arrays are rows of the matrix.
+There are different ways to transpose a matrix:
+    1) reflect the array over its main diagonal (which runs from top-left to bottom-right);
+    2) write the rows of the original matrix as columns of the new one;
+    3) write the columns of the original matrix as rows of the new one.
+*/
+
+public class Arrays {
+    public static Object[][] transpose(Object[][] matrix) {
+        int n = matrix.length;
+
+        if (n == 0) {
+            return matrix;
+        }
+
+        int m = matrix[0].length;
+        Object[][] result = new Object[m][n];
+
+        for (int i = 0; i < m; i++) {
+            result[i] = new Object[n];
+            for (int j = 0; j < n; j++) {
+                result[i][j] = matrix[j][i];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/bairamov/arraytranspose/ArraysTest.java
@@ -0,0 +1,38 @@
+package com.bairamov.arraytranspose;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(Parameterized.class)
+public class ArraysTest {
+    private Object[][] input;
+    private Object[][] expected;
+
+    public ArraysTest(Object[][] input, Object[][] expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return java.util.Arrays.asList(new Object[][]{
+                {new Object[][]{new Integer[]{1, 2, 3}, new String[]{":a", ":b", ":c"}},
+                        new Object[][]{new Object[]{1, ":a"}, new Object[]{2, ":b"}, new Object[]{3, ":c"}}},
+                {new Object[][]{new Integer[]{1, 3, 5}, new Integer[]{2, 4, 6}},
+                        new Object[][]{new Object[]{1, 2}, new Object[]{3, 4}, new Object[]{5, 6}}},
+                {new Object[][]{new Integer[]{1, 3, 5}},
+                        new Object[][]{new Object[]{1}, new Object[]{3}, new Object[]{5}}},
+                {new Object[][]{}, new Object[][]{}}
+        });
+    }
+
+    @Test
+    public void testTranspose() throws Exception {
+        assertArrayEquals(expected, Arrays.transpose(input));
+    }
+}
